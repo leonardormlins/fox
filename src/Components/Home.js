3bf24e9c512a6ff5ff8418d58b4972836b852a49
leonardormlins <leonardolins.ext@hotmail.com>
2020-05-12 15:54:28 -0300

    NavBar completed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,12 +4,25 @@ import './Styles/Home.css';
 function Home() {
   return (
     <div className="Home">
+      
       <section className="Navbar">
         <div className="Options">
-          <span className="material-icons md-light">home</span>
-          <span className="material-icons md-light">search</span>
-          <span className="material-icons md-light">people</span>
-          <span className="material-icons md-light">person</span>
+          <div>
+            <span className="material-icons md-light">home</span>
+            <div className="Icon-info">Home</div>
+          </div>
+          <div>
+            <span className="material-icons md-light">search</span>
+            <div>Search</div>
+          </div>
+          <div>
+            <span className="material-icons md-light">people</span>
+            <div>People</div>
+          </div>
+          <div>
+            <span className="material-icons md-light">person</span>
+            <div>Profile</div>
+          </div>
         </div>
       </section>
     </div>
