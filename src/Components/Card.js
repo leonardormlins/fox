dae91c1a68c265c4b73eb21e8019ecfcc968fb46
leonardormlins <leonardolins.ext@hotmail.com>
2020-05-21 00:57:45 -0300

    Card props already working

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import './Styles/Card.css';
 
-function Card() {
-  return (
-    <div className="Card">
-      <div className="Content-card">
-        <div className="Top-card">
-          <div className="Avatar-card"></div>
-          <span className="Username-card">BradPitt</span>
-        </div>
-        <div className="Media-card"></div>
-        <div className="Bottom-card">
-        <p className="Action">Like</p>
-        <p className="Action">Comment</p>
+class Card extends React.Component {
+  constructor(props){
+    super(props);
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <div className="Card">
+        <div className="Content-card">
+          <div className="Top-card">
+            <div className="Avatar-card"></div>
+            <span className="Username-card">{this.props.username}</span>
+          </div>
+          <div className="Media-card"></div>
+          <div className="Bottom-card">
+          <p className="Action">Like</p>
+          <p className="Action">Comment</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Card;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,45 +3,49 @@ import logoMinor from './Styles/logoMinor.svg';
 import Card from './Card';
 import './Styles/Home.css';
 
-function Home() {
-  return (
-    <div className="Home">
-      <header className="Header-app">
-        <div className="Minor-logo">
-          <img src={logoMinor} alt="logoMinor"/>
-        </div>
+class Home extends React.Component {
+  state = {
+    posts: ['Joao', 'Carlos', 'Pedro', 'Elias']
+  }
 
-        <div className="Header-line"></div>
-      </header>
-      <div className="Space-top"></div>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <div className="Space"></div>
-      <section className="Navbar">
-        <div className="Options">
-          <div>
-            <span className="material-icons md-light">home</span>
-            <div className="Icon-info">Home</div>
+  render() {
+    return (
+      <div className="Home">
+        <header className="Header-app">
+          <div className="Minor-logo">
+            <img src={logoMinor} alt="logoMinor"/>
           </div>
-          <div>
-            <span className="material-icons md-light">search</span>
-            <div>Search</div>
+
+          <div className="Header-line"></div>
+        </header>
+        <div className="Space-top"></div>
+
+        {this.state.posts.map(author => <Card key={author} username={author}/>)}
+        
+        <div className="Space"></div>
+        <section className="Navbar">
+          <div className="Options">
+            <div>
+              <span className="material-icons md-light">home</span>
+              <div className="Icon-info">Home</div>
+            </div>
+            <div>
+              <span className="material-icons md-light">search</span>
+              <div>Search</div>
+            </div>
+            <div>
+              <span className="material-icons md-light">people</span>
+              <div>People</div>
+            </div>
+            <div>
+              <span className="material-icons md-light">person</span>
+              <div>Profile</div>
+            </div>
           </div>
-          <div>
-            <span className="material-icons md-light">people</span>
-            <div>People</div>
-          </div>
-          <div>
-            <span className="material-icons md-light">person</span>
-            <div>Profile</div>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Home;
